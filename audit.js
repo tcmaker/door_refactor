54d85be9306567abc7bb58790bd698d659d7cbfa
Stephen Van Dahm <code@vandahm.com>
2020-06-24 11:35:03 -0500

    remove debug text

--- audit.js
+++ audit.js
@@ -1,6 +1,5 @@
 const webapp = require('./webapp')
 module.exports.message = function(text) {
-  console.log("BUTTS BUTTS BUTTS");
   console.log(text);
   webapp.io.sockets.emit('conmsg', text + '\r\n');
 }
